8272cdfd30ffb8ecac694cba48d94401d8c5c2f9
LowAiKiu <sang24092005@gmail.com>
2025-08-09 03:31:47 +0700

    Add files via upload

---
+++ lmao.js
@@ -0,0 +1,11 @@
+/*gallery.js*/
+
+function upDate(previewPic) {
+  document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
+  document.getElementById('image').innerHTML = previewPic.alt;
+}
+
+function unDo() {
+  document.getElementById('image').style.backgroundImage = "url('')";
+  document.getElementById('image').innerHTML = "Hover over an image to display.";
+}
